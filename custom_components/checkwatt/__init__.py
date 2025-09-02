2c8c30f1dfc71ebaad438fe2236e8e5cb888cd55
Mikael Johansson <46709289+twinor@users.noreply.github.com>
2025-09-02 21:01:35 +0200

    updated comment with correct random time addition

--- custom_components/checkwatt/__init__.py
+++ custom_components/checkwatt/__init__.py
@@ -432,7 +432,7 @@ class CheckwattCoordinator(DataUpdateCoordinator[CheckwattResp]):
                 if push_to_cw_rank:
                     if self.last_cw_rank_push is None or (
                         dt_util.now().time()
-                        >= time(11, self.random_offset)  # Wait until 11am +- 15 min
+                        >= time(11, self.random_offset)  # Wait until 11am + 0-14 min
                         and dt_util.start_of_local_day(dt_util.now())
                         != dt_util.start_of_local_day(self.last_cw_rank_push)
                     ):
